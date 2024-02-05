f586ef36c5857232b4433fca3853493d3eaa81d1
SebastianOrtiz95 <seba.colon@outlook.com>
2024-02-05 12:36:40 -0300

    modificar y crear pokemon

---
+++ PokedexWeb/ListaPokemon.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="ListaPokemon.aspx.cs" Inherits="PokedexWeb.ListaPokemon" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Lista Pokemon</h1>
+    <asp:GridView ID="dgvPokemons" runat="server" CssClass="table"></asp:GridView>
+</asp:Content>
---
+++ PokedexWeb/ListaPokemon.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+
+namespace PokedexWeb
+{
+    public partial class ListaPokemon : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            dgvPokemons.DataSource = negocio.listarconSP();
+            dgvPokemons.DataBind();
+        }
+    }
+}
